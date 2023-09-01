26cecd55ffc4f642ce2610f3d9be881f6c2262b0
vitorbizarra <vitorbizarracontato@gmail.com>
2023-09-01 01:06:05 +0000

    :construction: Cenário de testes método somar()

--- src/CalculadoraTestes.java
+++ src/CalculadoraTestes.java
@@ -3,11 +3,27 @@ public class CalculadoraTestes {
 
         Calculadora calc = new Calculadora();
 
-        // Cenário de testes 1: Soma de dois valores
+        System.out.println("Cenário de testes 1: Soma de dois valores inteiros (10)");
         int soma = calc.somar(3, 7);
         System.out.println(soma);
 
+        System.out.println("\nCenário de testes 2: Soma de um inteiro e zero (3)");
         soma = calc.somar(3, 0);
+        System.out.println(soma);
+
+
+        System.out.println("\nCenário de testes 3: Soma de um inteiro e um negativo (-4)");
+        soma = calc.somar(1, -5);
+        System.out.println(soma);
+
+
+        System.out.println("\nCenário de testes 4: Soma de dois negativos (-4)");
+        soma = calc.somar(-2, -2);
+        System.out.println(soma);
+
+
+        System.out.println("\nCenário de testes 5: Soma de um negativo e zero (-5)");
+        soma = calc.somar(-5, 0);
         System.out.println(soma);
     }
 }
